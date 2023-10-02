2c6bda01127e7b3755c16501f82493286cb7302b
mohamad.basyir <mohamad.basyir@mpsb.net>
2023-10-02 18:04:42 +0800

    Commit progress

--- src/main/java/com/basyir/projects/socialmedia/controller/HealthController.java
+++ src/main/java/com/basyir/projects/socialmedia/controller/HealthController.java
@@ -3,11 +3,14 @@ package com.basyir.projects.socialmedia.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HealthController {
     @GetMapping("/")
-    public ResponseEntity<String> ping() {
-        return new ResponseEntity<>("Hello and Welcome to Basyir Social Media!", HttpStatus.OK);
+    public ModelAndView ping() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");
+        return mav;
     }
 }
